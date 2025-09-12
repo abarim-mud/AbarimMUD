513d4212600a60b091f436ca8f7174e3d91f6706
Roman Shapiro <eleroman@mail.ru>
2025-09-12 08:07:25 +0700

    Fixed restart.sh

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pkill AbarimMUD
-rm -rf AbarimMUD.Launcher/bin/Release/net6.0/ubuntu.20.04-x64
-dotnet publish AbarimMUD.sln -c release -r ubuntu.20.04-x64
-nohup AbarimMUD.Launcher/bin/Release/net6.0/ubuntu.20.04-x64/AbarimMUD.Launcher Data &
+rm -rf AbarimMUD.Launcher/bin/Release
+dotnet publish AbarimMUD.sln -c release -r linux-x64
+nohup AbarimMUD.Launcher/bin/Release/net8.0/linux-x64/AbarimMUD.Launcher Data &
