a6c601d3f2039ee155c4b89dee9073509da09da5
Roman Shapiro <eleroman@mail.ru>
2025-09-12 07:43:36 +0700

    Remove DefaultCharacter from settings stored at repo

--- restart.sh
+++ restart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pkill AbarimMUD
+rm -rf AbarimMUD.Launcher/bin/Release/net6.0/ubuntu.20.04-x64
 dotnet publish AbarimMUD.sln -c release -r ubuntu.20.04-x64
 nohup AbarimMUD.Launcher/bin/Release/net6.0/ubuntu.20.04-x64/AbarimMUD.Launcher Data &
